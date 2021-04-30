2ee5e3780552d649fe2d6fe280e074365702b0ec
or man <Ormn1996@gmail.com>
2021-04-30 13:34:06 +0300

    javafx navigator

---
+++ JAVA/src/javafx/Navigator/GuiController.java
@@ -0,0 +1,14 @@
+package clientGui;
+
+/**
+ * <pre>
+ * interface for GUI to save them in the navigator,
+ * the init() function is called on load in the navigator
+ * </pre>
+ */
+public interface GuiController {
+
+	/** method to run after loading */
+	public default void init() {
+	}
+}
